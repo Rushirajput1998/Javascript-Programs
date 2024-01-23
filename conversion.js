7a175743eddd16da7875e072705f7fb989c8f03a
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-23 11:08:05 +0000

    Conversion or typeCoersion

---
+++ conversion.js
@@ -0,0 +1,19 @@
+let number=10;
+//console.log(typeof number);
+
+//let stringNumber=String(number);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+let someString="rishi";
+
+stringInNumner=Number(someString);
+//console.log(stringInNumner);//NaN
+//console.log(typeof stringInNumner);//Number
+
+let value=0;
+
+valueInBoolean=Boolean(value)
+
+console.log(valueInBoolean);//false
+console.log(typeof valueInBoolean);
