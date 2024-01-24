6ed27ad9a4f756bd59fea5b01935006124b1a564
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-24 16:25:19 +0000

    String Methods

---
+++ String.js
@@ -0,0 +1,14 @@
+const firstName="Rushikesh";
+const lastName="Shinde";
+
+// console.log(`My first Name is ${firstName} And last Name is ${lastName}`);
+
+var s="I love India";
+
+// console.log(s.concat("(Bharat)"));
+// console.log(s.charAt(0));
+// console.log(s.endsWith("India"));//true or false
+// console.log(s.includes("m"));//true or false
+// console.log(s.indexOf("l"));
+// console.log(s.lastIndexOf("o"));
+//console.log(s.length);
