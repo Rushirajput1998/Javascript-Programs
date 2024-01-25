35560bb4c67ad2c92e76c088e8c560bab037782b
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-25 16:25:23 +0000

    Math Methods

---
+++ Math.js
@@ -0,0 +1,19 @@
+const value=-10;
+
+// console.log(Math.abs(value));
+
+const num=23.6;
+// console.log(Math.round(num));
+// console.log(Math.random(num)*10);
+// console.log(Math.round(Math.random(num)*10));
+
+const num1=14.8;
+
+// console.log(Math.floor(num1));
+// console.log(Math.ceil(num1));
+
+const num2=16;
+console.log(Math.sqrt(num2));
+
+const num3=27
+console.log(Math.cbrt(num3));
