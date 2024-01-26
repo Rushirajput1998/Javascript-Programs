e2dd4aecbca341d7f06da2b64d4bae79b2c9c533
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-26 09:54:07 +0000

    Array Methods

---
+++ Array.js
@@ -0,0 +1,19 @@
+const arr=[0,1,2,3,4,5,6,7];
+
+console.log(arr);
+
+// console.log(arr.push(8));
+// console.log(arr);
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(arr.shift());
+// console.log(arr);
+
+// console.log(arr.unshift(8));
+// console.log(arr);
+
+// console.log(arr.length);
+
+// console.log(arr.join());//convert into a string
