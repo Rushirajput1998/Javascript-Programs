36b09aa7aa59deadabe64a39eceb01cd4517f1f8
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-31 03:49:11 +0000

    I add a Some Object Method

--- Objects.js
+++ Objects.js
@@ -28,6 +28,12 @@ myBiodata.greeting = function(){
     console.log(`My Pincode is ${this.Pincode}`);
 }
 
-console.log(myBiodata.greeting());
+// console.log(myBiodata.greeting());
 
 
+const obj1={a:"1",b:"2"};
+const obj2={c:"3",d:"4"};
+
+const obj3=Object.assign({},obj1,obj2);
+
+console.log(obj3);
