435b6f91338e6c3cdb15cf186c6cdd4c9aa42e9a
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-01-31 17:25:36 +0000

    Scopes In Js

---
+++ Scope.js
@@ -0,0 +1,23 @@
+if(true){
+    let a=10;
+    const b=20;
+    var c=30;
+}
+
+//  console.log(a);//localscope or blockScope
+//  console.log(b);//localscope or blockScope
+//  console.log(c);//global Level Scope
+
+
+function one(){
+    let username="Rushi";
+
+    function two(){
+        let password="rishirajput1998"
+        console.log(username);
+    }
+    // console.log(password);
+     two();
+}
+
+one();
