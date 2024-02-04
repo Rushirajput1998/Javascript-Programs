33e2998a1c2bc183c5bb7460aa38a1705ecdcc72
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-02-04 09:57:29 +0000

    Immidiately Invoked Function Expression

---
+++ iife.js
@@ -0,0 +1,12 @@
+//immidiately invoked function expression
+
+(function ex() {
+    console.log("welcome t the world")
+})();//semicolon is must when we use iife
+
+
+(() =>{
+    console.log("wecome to the arrow");
+}
+)()
+
