a67d0e1baae88ffc8b9e00e3a4704c3330e2efd6
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-02-04 09:59:47 +0000

    for loop and also break and continue statement

---
+++ loop.js
@@ -0,0 +1,30 @@
+// const a=[10,20,30,40,50];
+
+// for(let i=0;i<a.length;i++)
+// {
+//     console.log(a[i]);
+// }
+
+
+//break statement
+// for(let i=1;i<10;i++)
+// {
+//     if(i==5)
+//     {
+//         console.log("detetcted 5");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//continue Stetement
+
+for(let i=1;i<10;i++)
+{
+    if(i==5)
+    {
+        console.log("skip");
+        continue
+    }
+    console.log(i);
+}
