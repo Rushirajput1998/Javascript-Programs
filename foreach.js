bc1e1eada6682651be324f629d2176c98c0b275d
Rushikesh Shinde <132253358+Rushirajput1998@users.noreply.github.com>
2024-02-06 16:31:04 +0000

    i wrote a some examples by using foreach loop

---
+++ foreach.js
@@ -0,0 +1,10 @@
+const coding=["Java","Python","C++","C"];
+
+// coding.forEach(function (value){
+//     console.log(value);
+// })
+
+// coding.forEach((item,index,arr) =>{
+//     console.log(item,index,arr);
+// })
+
---
+++ truthy.js
@@ -0,0 +1,18 @@
+const useremail="rushi@gmail.com";
+
+if(useremail)
+{
+    console.log("got user email");
+}
+else{
+    console.log("dosent get user email");
+}
+
+
+//falsy values
+
+// false,0,-0,BIGINT,"",null,udefined,NaN
+
+//truthy values
+
+//"0",'false'," ",{},[],function(){}
